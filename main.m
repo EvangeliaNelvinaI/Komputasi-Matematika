c5362d79d435e10c70215cb8f99c01be8110dc5b
EvangeliaNelvinaI <evangelianelvinaisakandar@gmail.com>
2025-05-19 11:43:56 +0700

    Praktikum GA

--- main.m
+++ main.m
@@ -1,13 +1,23 @@
-% Definisikan fungsi
-f = inline('x^2-3','x');
+% Gen
+target = 'Evangelia'
+eva = length(target);
+genes = create_genes(eva);
 
-X = [1 2];
-%panggil fungsi bagi dua
-% [x, galat] = bagidua(f,X);
-% disp(x);
-% disp(galat);
+%fitnes
+fitnes = calculate_fitness(genes,eva);
 
-%panggil fungsi posisi palsu
-[x, galat] = PosisiPalsu(f, X);
-disp(x);
-disp(galat);
+%populasi
+population_size = 10;
+population = create_population(target,population_size)
+%selection
+[perent1, perent2] = selection(population)
+%crossover
+[child1, child2] = crossover (perent1, perent2)
+%mutation
+mutation_rate = 0.5;
+mutant1 = mutation(child1, mutation_rate)
+mutant2 = mutation(child2, mutation_rate)
+
+%regeneration
+children = [mutant1 mutant2];
+population = regeneration(children,population)
