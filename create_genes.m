f7fa22e593cc37a3f047b49659e6fd0a3f3c72c6
EvangeliaNelvinaI <evangelianelvinaisakandar@gmail.com>
2025-07-14 09:17:16 +0700

    Coding peper

--- create_genes.m
+++
@@ -1,4 +0,0 @@
-function genes = create_genes(eva)
-random_number = randi ([32,126],1,eva);
-genes = char(random_number);
-end
--- crossover.m
+++
@@ -1,9 +0,0 @@
-function [child1, child2] = crossover (perent1, perent2)
-
-child1 = perent1;
-child2 = perent2;
-cp = round(length(perent1.genes)/2);
-
-child1.genes(1:cp) = perent2.genes(1:cp);
-child2.genes(1:cp) = perent1.genes(1:cp);
-end
